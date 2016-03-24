68cb7d02858e5d584aff19fc19079369597ef38e
andres-mancera <andres-mancera@users.noreply.github.com>
2016-03-24 09:13:57 -0700

    Adding body task to the packet_sequence

--- testbench/packet_sequence.sv
+++ testbench/packet_sequence.sv
@@ -1,8 +1,22 @@
+`ifndef PACKET_SEQUENCE_SV
+`define PACKET_SEQUENCE_SV
+
 `include "packet.sv"
 
 
 class packet_sequence extends uvm_sequence #(packet);
 
-   `uvm_object_utils(packet_sequence)
+  `uvm_object_utils(packet_sequence)
+
+  function new(input string name="packet_sequence");
+    super.new(name);
+    `uvm_info( get_name(), $sformatf("Hierarchy: %m"), UVM_HIGH )
+  endfunction : new
+
+  virtual task body();
+    `uvm_do(req);
+  endtask : body
 
 endclass : packet_sequence
+
+`endif // PACKET_SEQUENCE_SV

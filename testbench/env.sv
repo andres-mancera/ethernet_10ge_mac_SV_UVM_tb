76976e719d2139b35998a3b71e08945e07705340
andres-mancera <andres-mancera@users.noreply.github.com>
2016-03-24 10:21:09 -0700

    Env now instantiates the input_tx_agent.

--- testbench/env.sv
+++ testbench/env.sv
@@ -1,9 +1,25 @@
+`ifndef ENV__SV
+`define ENV__SV
+
+`include "packet_tx_agent.sv"
+
+
 class env extends uvm_env;
 
-   `uvm_component_utils(env)
+  packet_tx_agent   pkt_tx_agent;
 
-   function new(input string name, input uvm_component parent);
-      super.new(name, parent);
-   endfunction
+  `uvm_component_utils(env)
+
+  function new(input string name, input uvm_component parent);
+    super.new(name, parent);
+  endfunction : new
+
+
+  virtual function void build_phase( input uvm_phase phase );
+    super.build_phase( phase );
+    pkt_tx_agent = packet_tx_agent::type_id::create( "pkt_tx_agent", this );
+  endfunction : build_phase
 
 endclass : env
+
+`endif //ENV__SV

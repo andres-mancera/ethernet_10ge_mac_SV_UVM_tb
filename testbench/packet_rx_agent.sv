4b68bc9044c1f0daa0cd4a70131026a787cff98d
andres-mancera <andres-mancera@users.noreply.github.com>
2016-03-25 10:09:17 -0700

    First cut of the packet_rx agent and monitor.

---
+++ testbench/packet_rx_agent.sv
@@ -0,0 +1,33 @@
+`ifndef PACKET_RX_AGENT__SV
+`define PACKET_RX_AGENT__SV
+
+`include "packet_rx_monitor.sv"
+
+
+class packet_rx_agent extends uvm_agent;
+
+  packet_rx_monitor             pkt_rx_mon;
+  uvm_analysis_port #(packet)   ap_rx_agent;
+
+  `uvm_component_utils( packet_rx_agent )
+
+  function new( input string name="packet_rx_agent", input uvm_component parent );
+    super.new( name, parent );
+  endfunction : new
+
+
+  virtual function void build_phase( input uvm_phase phase );
+    super.build_phase( phase );
+    pkt_rx_mon  = packet_rx_monitor::type_id::create( "pkt_rx_mon", this );    
+    ap_rx_agent = new ( "ap_rx_agent", this );
+  endfunction : build_phase
+
+
+  virtual function void connect_phase( input uvm_phase phase );
+    super.connect_phase( phase );
+    this.ap_rx_agent = pkt_rx_mon.ap_rx_mon;
+  endfunction : connect_phase
+
+endclass : packet_rx_agent
+
+`endif  // PACKET_RX_AGENT__SV
---
+++ testbench/packet_rx_monitor.sv
@@ -0,0 +1,37 @@
+`ifndef PACKET_RX_MONITOR__SV
+`define PACKET_RX_MONITOR__SV
+
+
+class packet_rx_monitor extends uvm_monitor #(packet);
+
+  virtual xge_mac_interface     mon_vi;
+  uvm_analysis_port #(packet)   ap_rx_mon;
+
+  `uvm_component_utils( packet_rx_monitor )
+
+  function new(input string name="packet_rx_monitor", input uvm_component parent);
+    super.new(name, parent);
+  endfunction : new
+
+
+  virtual function void build_phase(input uvm_phase phase);
+    super.build_phase(phase);
+    ap_rx_mon = new ( "ap_rx_mon", this );
+    uvm_config_db#(virtual xge_mac_interface)::get(this, "", "mon_vi", mon_vi);
+    if ( mon_vi==null )
+      `uvm_fatal(get_name(), "Virtual Interface for monitor not set!");
+  endfunction : build_phase
+
+
+  virtual task run_phase(input uvm_phase phase);
+    packet  rcv_pkt;
+
+    `uvm_info( get_name(), $sformatf("HIERARCHY: %m"), UVM_HIGH);
+    // FIXME: Add all the code to collect transactions.
+
+  endtask : run_phase
+
+endclass : packet_rx_monitor
+
+`endif  //PACKET_RX_MONITOR__SV
+

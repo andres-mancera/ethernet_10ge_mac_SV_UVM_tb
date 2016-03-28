ed100de83f8541a75ab7ffe6ec8394b47ceef9d7
andres-mancera <andres-mancera@users.noreply.github.com>
2016-03-28 10:42:11 -0700

    Integrating the scbd into the environment class.

--- testbench/env.sv
+++ testbench/env.sv
@@ -3,11 +3,14 @@
 
 `include "packet_tx_agent.sv"
 `include "packet_rx_agent.sv"
+`include "scoreboard.sv"
+
 
 class env extends uvm_env;
 
   packet_tx_agent   pkt_tx_agent;
   packet_rx_agent   pkt_rx_agent;
+  scoreboard        scbd;
 
   `uvm_component_utils(env)
 
@@ -20,8 +23,16 @@ class env extends uvm_env;
     super.build_phase( phase );
     pkt_tx_agent = packet_tx_agent::type_id::create( "pkt_tx_agent", this );
     pkt_rx_agent = packet_rx_agent::type_id::create( "pkt_rx_agent", this );
+    scbd         = scoreboard::type_id::create( "scbd", this );
   endfunction : build_phase
+
+
+  virtual function void connect_phase ( input uvm_phase phase );
+    super.connect_phase( phase );
+    pkt_tx_agent.ap_tx_agent.connect( scbd.from_pkt_tx_agent );
+    pkt_rx_agent.ap_rx_agent.connect( scbd.from_pkt_rx_agent );
+  endfunction : connect_phase
 
 endclass : env
 
-`endif //ENV__SV
+`endif  //ENV__SV

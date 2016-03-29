e05dc111f8c273a86dd62408fefd243e20a7d968
andres-mancera <andres-mancera@users.noreply.github.com>
2016-03-29 14:18:40 -0700

    Wishbone EOT sequence will happen after packet_tx sequence.

--- testbench/virtual_sequence.sv
+++ testbench/virtual_sequence.sv
@@ -7,9 +7,10 @@ class virtual_sequence extends uvm_sequence;
   `uvm_object_utils(virtual_sequence)
   `uvm_declare_p_sequencer(virtual_sequencer)
 
-  reset_sequence    seq_rst;
-  wishbone_sequence seq_wshbn;
-  packet_sequence   seq_pkt;
+  reset_sequence            seq_rst;
+  wishbone_init_sequence    seq_init_wshbn;
+  wishbone_eot_sequence     seq_eot_wshbn;
+  packet_sequence           seq_pkt;
 
   function new(input string name="virtual_sequence");
     super.new(name);
@@ -18,9 +19,9 @@ class virtual_sequence extends uvm_sequence;
 
 
   virtual task body();
-//    `uvm_do_on( seq_rst, p_sequencer.seqr_rst );
-//    `uvm_do_on( seq_wshbn,  p_sequencer.seqr_wshbn );
     `uvm_do_on( seq_pkt, p_sequencer.seqr_tx_pkt );
+    #1000000;
+    `uvm_do_on( seq_eot_wshbn, p_sequencer.seqr_wshbn );
   endtask : body
 
 

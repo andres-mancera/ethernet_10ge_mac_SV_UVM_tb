7d7dbfa3936698ac2d7c2bc0d19021ba2961f8d9
andres-mancera <andres-mancera@users.noreply.github.com>
2016-03-29 18:11:07 -0700

    Scoreboard will check wishbone read transactions to make sure there are no interrupts

--- testbench/scoreboard.sv
+++ testbench/scoreboard.sv
@@ -8,9 +8,12 @@ class scoreboard extends uvm_scoreboard;
 
   packet        pkt_tx_agent_q [$];
   packet        pkt_rx_agent_q [$];
+  wishbone_item wshbn_read_q [$];
   int unsigned  m_matches;
   int unsigned  m_mismatches;
+  int unsigned  m_dut_errors;
   uvm_event     check_packet_event;
+  uvm_event     check_wshbn_event;
 
   `uvm_component_utils( scoreboard )
 
@@ -18,6 +21,8 @@ class scoreboard extends uvm_scoreboard;
   uvm_analysis_imp_from_pkt_tx_agent #( packet, scoreboard )    from_pkt_tx_agent;
   `uvm_analysis_imp_decl( _from_pkt_rx_agent )
   uvm_analysis_imp_from_pkt_rx_agent #( packet, scoreboard )    from_pkt_rx_agent;
+  `uvm_analysis_imp_decl( _from_wshbn_agent )
+  uvm_analysis_imp_from_wshbn_agent #( wishbone_item, scoreboard )  from_wshbn_agent;
 
 
   function new( input string name="scoreboard", input uvm_component parent );
@@ -28,9 +33,12 @@ class scoreboard extends uvm_scoreboard;
     super.build_phase( phase );
     m_matches       = 0;
     m_mismatches    = 0;
+    m_dut_errors    = 0;
     from_pkt_tx_agent   = new ("from_pkt_tx_agent", this);
     from_pkt_rx_agent   = new ("from_pkt_rx_agent", this);
+    from_wshbn_agent    = new ("from_wshbn_agent", this );
     check_packet_event  = new ( "check_packet_event" );
+    check_wshbn_event   = new ( "check_wshbn_event" );
   endfunction : build_phase
 
 
@@ -45,12 +53,26 @@ class scoreboard extends uvm_scoreboard;
   endfunction : write_from_pkt_rx_agent
 
 
+  virtual function write_from_wshbn_agent( wishbone_item wshbn_xtxn );
+    wshbn_read_q.push_back( wshbn_xtxn );
+    check_wshbn_event.trigger( );
+  endfunction : write_from_wshbn_agent
+
+
   virtual task check_packet( );
     forever begin
       check_packet_event.wait_trigger( );
       check_packet_queues( );
     end
   endtask : check_packet
+
+
+  virtual task check_wishbone_trans( );
+    forever begin
+      check_wshbn_event.wait_trigger( );
+      check_wshbn_queue( );
+    end
+  endtask : check_wishbone_trans
 
 
   virtual function void check_packet_queues() ;
@@ -120,10 +142,38 @@ class scoreboard extends uvm_scoreboard;
   endfunction : compare_payload_bytes
 
 
+  virtual function void check_wshbn_queue( );
+    wishbone_item   xtxn;
+    int unsigned    error;
+    while ( wshbn_read_q.size() ) begin
+      error = 0;
+      xtxn = wshbn_read_q.pop_front( );
+      if ( xtxn.xtxn_n==wishbone_item::WRITE ) begin
+        `uvm_info( get_name(), $psprintf( "WISHBONE WR XTXN - No checking done" ), UVM_HIGH )
+      end
+      else if ( xtxn.xtxn_n==wishbone_item::READ ) begin
+        // Make sure there are no interrupts
+        if ( (xtxn.xtxn_addr==8'h08 || xtxn.xtxn_addr==8'h0C) && xtxn.xtxn_data!=32'h0 ) begin
+          `uvm_error( get_name(), $psprintf( "WISHBONE RD XTXN - Error" ) )
+          `uvm_error( get_name(), $psprintf( "RD_ADDR=0x%0x, Exp RD_DATA=0x%0x, Act RD_DATA=0x%0x",
+                    xtxn.xtxn_addr, 32'h0, xtxn.xtxn_data ) )
+          error++;
+        end
+        else begin
+          `uvm_info( get_name(), $psprintf( "RD_ADDR=0x%0x, Exp RD_DATA=0x%0x, Act RD_DATA=0x%0x",
+                    xtxn.xtxn_addr, 32'h0, xtxn.xtxn_data ), UVM_HIGH )
+        end
+      end
+      if ( error )  m_dut_errors++;
+    end
+  endfunction : check_wshbn_queue
+
+
   task run_phase ( input uvm_phase phase );
     `uvm_info( get_name(), $sformatf("HIERARCHY: %m"), UVM_HIGH);
     fork
       check_packet( );
+      check_wishbone_trans( );
     join_none
   endtask : run_phase
 
@@ -132,7 +182,8 @@ class scoreboard extends uvm_scoreboard;
     super.final_phase( phase );
     `uvm_info( get_name( ), $sformatf( "FINAL: Packet Matches   =%0d", m_matches ), UVM_LOW )
     `uvm_info( get_name( ), $sformatf( "FINAL: Packet Mismatches=%0d", m_mismatches), UVM_LOW )
-    if ( m_mismatches )
+    `uvm_info( get_name( ), $sformatf( "FINAL: Wishbone Errors  =%0d", m_dut_errors), UVM_LOW )
+    if ( m_mismatches || m_dut_errors )
       `uvm_error( get_name(), "********** TEST FAILED **********" )
     else
       `uvm_info ( get_name(), "********** TEST PASSED **********", UVM_NONE )

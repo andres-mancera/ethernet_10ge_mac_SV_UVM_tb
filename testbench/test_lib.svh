8026d89be24b3cfd14d8581439bcdd6229ae0930
andres-mancera <andres-mancera@users.noreply.github.com>
2016-03-30 18:12:50 -0700

    Updating the test library

--- testbench/test_lib.svh
+++ testbench/test_lib.svh
@@ -4,5 +4,8 @@
 `include "../testcases/bringup_packet_test.sv"
 `include "../testcases/oversized_packet_test.sv"
 `include "../testcases/undersized_packet_test.sv"
+`include "../testcases/small_large_packet_test.sv"
+`include "../testcases/small_ipg_packet_test.sv"
+`include "../testcases/zero_ipg_packet_test.sv"
 
 `endif  // TEST_LIB__SVH

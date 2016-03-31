10dd8e9a7e5fdc455c5ed391bf26dc8a3c25c9dc
andres-mancera <andres-mancera@users.noreply.github.com>
2016-03-31 16:32:36 -0700

    Adding idle cycle drive code to the wishbone driver.

--- testbench/wishbone_driver.sv
+++ testbench/wishbone_driver.sv
@@ -23,32 +23,37 @@ class wishbone_driver extends uvm_driver #(wishbone_item);
 
   virtual task run_phase(input uvm_phase phase);
     `uvm_info( get_name(), $sformatf("HIERARCHY: %m"), UVM_HIGH);
-    // Initial assignment for wishbone interface signals
-    drv_vi.drv_cb.wb_adr_i  <= $urandom_range(255,0);
-    drv_vi.drv_cb.wb_cyc_i  <= 1'b0;
-    drv_vi.drv_cb.wb_stb_i  <= 1'b0;
-    drv_vi.drv_cb.wb_dat_i  <= $urandom;
-    drv_vi.drv_cb.wb_we_i   <= 1'b0;
-
     forever begin
-      seq_item_port.get_next_item(req);
-      `uvm_info( get_name(), $psprintf("Wishbone Transaction: \n%0s", req.sprint()), UVM_HIGH)
-      @(drv_vi.drv_cb);
-      drv_vi.drv_cb.wb_adr_i  <= req.xtxn_addr;
-      drv_vi.drv_cb.wb_cyc_i  <= 1'b1;
-      drv_vi.drv_cb.wb_stb_i  <= 1'b1;
-      drv_vi.drv_cb.wb_dat_i  <= req.xtxn_data;
-      drv_vi.drv_cb.wb_we_i   <= req.xtxn_n;
-      repeat (2)
+      seq_item_port.try_next_item(req);
+      if ( req == null ) begin
+        // Send random idle
         @(drv_vi.drv_cb);
-      drv_vi.drv_cb.wb_adr_i  <= $urandom_range(255,0);
-      drv_vi.drv_cb.wb_cyc_i  <= 1'b0;
-      drv_vi.drv_cb.wb_stb_i  <= 1'b0;
-      drv_vi.drv_cb.wb_dat_i  <= $urandom;
-      drv_vi.drv_cb.wb_we_i   <= 1'b0;
-      repeat (20)
+        drv_vi.drv_cb.wb_adr_i  <= $urandom_range(255,0);
+        drv_vi.drv_cb.wb_cyc_i  <= 1'b0;
+        drv_vi.drv_cb.wb_stb_i  <= 1'b0;
+        drv_vi.drv_cb.wb_dat_i  <= $urandom;
+        drv_vi.drv_cb.wb_we_i   <= 1'b0;
+      end
+      else begin
+        `uvm_info( get_name(), $psprintf("Wishbone Transaction: \n%0s", req.sprint()), UVM_HIGH)
         @(drv_vi.drv_cb);
-      seq_item_port.item_done();
+        drv_vi.drv_cb.wb_adr_i  <= req.xtxn_addr;
+        drv_vi.drv_cb.wb_cyc_i  <= 1'b1;
+        drv_vi.drv_cb.wb_stb_i  <= 1'b1;
+        drv_vi.drv_cb.wb_dat_i  <= req.xtxn_data;
+        drv_vi.drv_cb.wb_we_i   <= req.xtxn_n;
+        repeat (2)
+          @(drv_vi.drv_cb);
+        repeat (20) begin
+          drv_vi.drv_cb.wb_adr_i  <= $urandom_range(255,0);
+          drv_vi.drv_cb.wb_cyc_i  <= 1'b0;
+          drv_vi.drv_cb.wb_stb_i  <= 1'b0;
+          drv_vi.drv_cb.wb_dat_i  <= $urandom;
+          drv_vi.drv_cb.wb_we_i   <= 1'b0;
+          @(drv_vi.drv_cb);
+        end
+        seq_item_port.item_done();
+      end
     end
   endtask : run_phase
 
